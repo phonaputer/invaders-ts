3765b3fefaf0604d2908b7f9aa399269ff9404c9
Phonaputer <njerome@uwalumni.com>
2026-09-23 18:43:53 +0900

    Add audio loading to invasion scene.

--- src/scenes/invasion/constants.ts
+++ src/scenes/invasion/constants.ts
@@ -3,3 +3,12 @@ import { GAME_HEIGHT } from "@src/framework/constants";
 export const GROUND_HEIGHT = GAME_HEIGHT - 23;
 
 export const SPRITE_SHEET_IMG_ID = "spritesheet";
+
+export const ALIEN_EXPLOSION_AUDIO = "alien-explosion";
+export const ALIEN_ATTACK_AUDIO = "alien-attack";
+export const ARP1_AUDIO = "arp1";
+export const ARP2_AUDIO = "arp2";
+export const ARP3_AUDIO = "arp3";
+export const ARP4_AUDIO = "arp4";
+export const PLAYER_EXPLOSION_AUDIO = "player-explosion";
+export const PLAYER_ATTACK_AUDIO = "player-attack";
--- src/scenes/invasion/invasion-scene.ts
+++ src/scenes/invasion/invasion-scene.ts
@@ -1,5 +1,15 @@
 import type { LoadAssetsCtx, SetSceneCtx } from "@src/framework/scene";
-import { SPRITE_SHEET_IMG_ID } from "@src/scenes/invasion/constants";
+import {
+  ALIEN_ATTACK_AUDIO,
+  ALIEN_EXPLOSION_AUDIO,
+  ARP1_AUDIO,
+  ARP2_AUDIO,
+  ARP3_AUDIO,
+  ARP4_AUDIO,
+  PLAYER_ATTACK_AUDIO,
+  PLAYER_EXPLOSION_AUDIO,
+  SPRITE_SHEET_IMG_ID,
+} from "@src/scenes/invasion/constants";
 import { setupGame } from "@src/scenes/invasion/entities/game";
 import AnimationSystem from "@src/scenes/invasion/systems/animation";
 import CallbackOnTimeoutSystem from "@src/scenes/invasion/systems/callback-on-timeout";
@@ -15,11 +25,28 @@ import SpriteRenderingSystem from "@src/scenes/invasion/systems/sprite-rendering
 import VelocitySystem from "@src/scenes/invasion/systems/velocity";
 import SpriteTextRenderer from "@src/scenes/sprite-text-renderer";
 
-import spriteSheetSrc from "@src/scenes/invasion/assets/space_invaders.png";
+import alienExplosionAudio from "@src/scenes/invasion/assets/alien_explosion.wav";
+import alienShotAudio from "@src/scenes/invasion/assets/alien_shot.wav";
+import arp1Audio from "@src/scenes/invasion/assets/arp1.wav";
+import arp2Audio from "@src/scenes/invasion/assets/arp2.wav";
+import arp3Audio from "@src/scenes/invasion/assets/arp3.wav";
+import arp4Audio from "@src/scenes/invasion/assets/arp4.wav";
+import playerExplosionAudio from "@src/scenes/invasion/assets/player_explosion.wav";
+import playerShotAudio from "@src/scenes/invasion/assets/player_shot.wav";
+import spriteSheetImage from "@src/scenes/invasion/assets/space_invaders.png";
 
 const InvasionScene = {
   loadAssets: (ctx: LoadAssetsCtx): void => {
-    ctx.assetLoader.loadImage(SPRITE_SHEET_IMG_ID, spriteSheetSrc);
+    ctx.assetLoader.loadImage(SPRITE_SHEET_IMG_ID, spriteSheetImage);
+
+    ctx.assetLoader.loadAudio(ALIEN_EXPLOSION_AUDIO, alienExplosionAudio);
+    ctx.assetLoader.loadAudio(ALIEN_ATTACK_AUDIO, alienShotAudio);
+    ctx.assetLoader.loadAudio(ARP1_AUDIO, arp1Audio);
+    ctx.assetLoader.loadAudio(ARP2_AUDIO, arp2Audio);
+    ctx.assetLoader.loadAudio(ARP3_AUDIO, arp3Audio);
+    ctx.assetLoader.loadAudio(ARP4_AUDIO, arp4Audio);
+    ctx.assetLoader.loadAudio(PLAYER_EXPLOSION_AUDIO, playerExplosionAudio);
+    ctx.assetLoader.loadAudio(PLAYER_ATTACK_AUDIO, playerShotAudio);
   },
   setScene: (ctx: SetSceneCtx): void => {
     ctx.systemRegistry.registerTickSystem(new CollisionDetectionSystem());
